8959276a808d33d1a7d675a6e306b68d1f230725
Xue Jiao <jiao.xue@seamoney.com>
2021-10-03 22:31:45 +0800

    impl Get GetTwo

--- consistent.go
+++ consistent.go
@@ -1,6 +1,7 @@
 package consistent
 
 import (
+	"errors"
 	"fmt"
 	"hash/crc32"
 	"sort"
@@ -11,6 +12,8 @@ type uints []uint32
 func (x uints) Len() int           { return len(x) }
 func (x uints) Less(i, j int) bool { return x[i] < x[j] }
 func (x uints) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+var ErrEmptyCircle = errors.New("empty circle")
 
 type Consistent struct {
 	circle           map[uint32]string
@@ -41,6 +44,60 @@ func (c *Consistent) Remove(elt string) {
 	}
 	c.updateSortedHashes()
 	c.count--
+}
+
+// Get returns an element close to where name hashes to in the circle.
+func (c *Consistent) Get(name string) (string, error) {
+	if len(c.circle) == 0 {
+		return "", ErrEmptyCircle
+	}
+	key := c.hashKey(name)
+	f := func(x int) bool {
+		return c.sortedHashes[x] > key
+	}
+	i := sort.Search(len(c.sortedHashes), f)
+	if i >= len(c.sortedHashes) {
+		i = 0
+	}
+	return c.circle[c.sortedHashes[i]], nil
+}
+
+// Get two returns the two closest distinct elements to the name input in the circle.
+func (c *Consistent) GetTwo(name string) (string, string, error) {
+	if len(c.circle) == 0 {
+		return "", "", ErrEmptyCircle
+	}
+	key := c.hashKey(name)
+	f := func(x int) bool {
+		return c.sortedHashes[x] > key
+	}
+	i := sort.Search(len(c.sortedHashes), f)
+	if i >= len(c.sortedHashes) {
+		i = 0
+	}
+	a := c.circle[c.sortedHashes[i]]
+
+	if c.count == 1 {
+		return a, "", nil
+	}
+
+	start := i
+	i++
+	if i >= len(c.sortedHashes) {
+		i = 0
+	}
+	var b string
+	for i != start {
+		b = c.circle[c.sortedHashes[i]]
+		if b != a {
+			break
+		}
+		i++
+		if i >= len(c.sortedHashes) {
+			i = 0
+		}
+	}
+	return a, b, nil
 }
 
 func (c *Consistent) hashKey(key string) uint32 {
